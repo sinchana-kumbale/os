3ff0249cd6f1031677eb4f43159acc40fb9b4ffd
sinchana-kumbale <sinchana.kumbale@gmail.com>
2022-10-24 11:11:49 +0530

    Added flow

--- Kernel.cs
+++ Kernel.cs
@@ -28,7 +28,25 @@ namespace OS
             }
             if (input == "fcfs")
             {
-                Console.WriteLine("This will work - 1");
+                //Console.WriteLine("This will work - 1");
+                int[] arr = new int[10];
+                int i;
+                Console.Write("\n\nRead and Print elements of an array:\n");
+                Console.Write("-----------------------------------------\n");
+
+                Console.Write("Input 10 elements in the array :\n");
+                for (i = 0; i < 10; i++)
+                {
+                    Console.Write("element - {0} : ", i);
+                    arr[i] = Convert.ToInt32(Console.ReadLine());
+                }
+
+                Console.Write("\nElements in array are: ");
+                for (i = 0; i < 10; i++)
+                {
+                    Console.Write("{0}  ", arr[i]);
+                }
+                Console.Write("\n");
             }
             if (input == "p_np")
             {
